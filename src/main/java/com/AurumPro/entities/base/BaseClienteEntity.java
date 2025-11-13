7302e0e405607015f468cb2e8f60c4e10be2e180
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-13 15:19:49 -0300

    feat: Adição da annotation get

--- src/main/java/com/AurumPro/entities/base/BaseClienteEntity.java
+++ src/main/java/com/AurumPro/entities/base/BaseClienteEntity.java
@@ -4,26 +4,41 @@ import com.AurumPro.entities.empresa.Empresa;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import lombok.Setter;
 
 @MappedSuperclass
 public abstract class BaseClienteEntity extends BaseEntity{
 
+    @Getter
     @Setter
     private String email;
+
+    @Getter
     @Setter
     private String telefone;
 
+    @Getter
     @Setter
     private String cep;
+
+    @Getter
     @Setter
     private String rua;
+
+    @Getter
     @Setter
     private String bairro;
+
+    @Getter
     @Setter
     private String cidade;
+
+    @Getter
     @Setter
     private String estado;
+
+    @Getter
     @Setter
     private Integer numero;
 
--- src/main/java/com/AurumPro/entities/base/BaseEntity.java
+++ src/main/java/com/AurumPro/entities/base/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import lombok.Setter;
 
 @MappedSuperclass
@@ -11,8 +12,10 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
     private Long id;
 
+    @Getter
     @Setter
     private String nome;
 }
