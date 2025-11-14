2b8fb4809d0587567fecbfadfe336ad1e203a02d
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-14 09:10:15 -0300

    refactor: Inserção das annotations Get e Set em toda classe

--- src/main/java/com/AurumPro/entities/base/BaseClienteEntity.java
+++ src/main/java/com/AurumPro/entities/base/BaseClienteEntity.java
@@ -7,40 +7,26 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseClienteEntity extends BaseEntity{
 
-    @Getter
-    @Setter
     private String email;
 
-    @Getter
-    @Setter
     private String telefone;
 
-    @Getter
-    @Setter
     private String cep;
 
-    @Getter
-    @Setter
     private String rua;
 
-    @Getter
-    @Setter
     private String bairro;
 
-    @Getter
-    @Setter
     private String cidade;
 
-    @Getter
-    @Setter
     private String estado;
 
-    @Getter
-    @Setter
-    private Integer numero;
+    private String numero;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "empresaId")
