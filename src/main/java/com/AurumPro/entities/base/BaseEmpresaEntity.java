b76e3ba91cc66b1c437aca5ead1249cd5d7594b6
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-17 09:32:27 -0300

    refactor: Adicionando a annotation setter

--- src/main/java/com/AurumPro/entities/base/BaseEmpresaEntity.java
+++ src/main/java/com/AurumPro/entities/base/BaseEmpresaEntity.java
@@ -4,8 +4,10 @@ import com.AurumPro.entities.empresa.Empresa;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.ManyToOne;
+import lombok.Setter;
 
 @MappedSuperclass
+@Setter
 public abstract class BaseEmpresaEntity extends BaseEntity{
 
     @ManyToOne(optional = false)
