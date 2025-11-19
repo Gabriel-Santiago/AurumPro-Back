290eecc3b5dc3a99dc54b06628330f85efcf7775
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-19 15:57:21 -0300

    refactor: Adição do getter

--- src/main/java/com/AurumPro/entities/base/BaseComponenteEntity.java
+++ src/main/java/com/AurumPro/entities/base/BaseComponenteEntity.java
@@ -4,9 +4,11 @@ import com.AurumPro.entities.empresa.Empresa;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import lombok.Setter;
 
 @MappedSuperclass
+@Getter
 @Setter
 public abstract class BaseComponenteEntity extends BaseEntity{
 
