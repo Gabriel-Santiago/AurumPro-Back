10263d4bc8b47c919e077da0049f53513d0cce5d
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-19 10:20:42 -0300

    refactor: Retirada da annotation NoArgsConstructor

--- src/main/java/com/AurumPro/entities/componentes/Convenio.java
+++ src/main/java/com/AurumPro/entities/componentes/Convenio.java
@@ -5,14 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "convenio")
 @AllArgsConstructor
 @Getter
-@NoArgsConstructor
 @Setter
 public class Convenio extends BaseComponenteEntity {
 
