d5f54c81b5ceafab02b3b91ef63b734c3b68eff4
gabriel <gabrielsrmj@alu.ufc.br>
2025-11-25 15:24:54 -0300

    refactor: Retirei id da proposta, já que o custo é criado antes da proposta

--- src/main/java/com/AurumPro/dtos/componentes/custo/CreateCustoDTO.java
+++ src/main/java/com/AurumPro/dtos/componentes/custo/CreateCustoDTO.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 
 public record CreateCustoDTO(
         Long empresaId,
-        Long propostaId,
         String nome,
         BigDecimal valor
 ) {
--- src/main/java/com/AurumPro/services/componentes/CustoService.java
+++ src/main/java/com/AurumPro/services/componentes/CustoService.java
@@ -3,12 +3,9 @@ package com.AurumPro.services.componentes;
 import com.AurumPro.dtos.componentes.custo.CreateCustoDTO;
 import com.AurumPro.entities.componentes.Custo;
 import com.AurumPro.entities.empresa.Empresa;
-import com.AurumPro.entities.proposta.Proposta;
 import com.AurumPro.exceptions.empresa.EmpresaNotFoundException;
-import com.AurumPro.exceptions.proposta.PropostaNotFoundException;
 import com.AurumPro.repositories.componentes.CustoRepository;
 import com.AurumPro.repositories.empresa.EmpresaRepository;
-import com.AurumPro.repositories.proposta.PropostaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,14 +13,11 @@ public class CustoService{
 
     private final CustoRepository repository;
     private final EmpresaRepository empresaRepository;
-    private final PropostaRepository propostaRepository;
 
     public CustoService(CustoRepository repository,
-                        EmpresaRepository empresaRepository,
-                        PropostaRepository propostaRepository) {
+                        EmpresaRepository empresaRepository) {
         this.repository = repository;
         this.empresaRepository = empresaRepository;
-        this.propostaRepository = propostaRepository;
     }
 
     public void createCusto(CreateCustoDTO dto){
@@ -31,16 +25,11 @@ public class CustoService{
                 .findById(dto.empresaId())
                 .orElseThrow(EmpresaNotFoundException::new);
 
-        Proposta proposta = propostaRepository
-                .findById(dto.propostaId())
-                .orElseThrow(PropostaNotFoundException::new);
-
         Custo custo = new Custo();
         custo.setNome(dto.nome());
         custo.setValor(dto.valor());
 
         custo.setEmpresa(empresa);
-        custo.setProposta(proposta);
 
         repository.save(custo);
     }
